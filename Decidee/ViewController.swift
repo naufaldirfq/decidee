599b8c279a9fd77513d00a1a4087a586911c143b
Naufaldi Athallah Rifqi <naufaldi.athallah@ui.ac.id>
2021-05-04 14:00:17 +0700

    Initial Commit

---
+++ Decidee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Decidee
+//
+//  Created by Naufaldi Athallah Rifqi on 04/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
